a021e10faedb64801886a1211cadb2b8cb5de019
幽离 <90978564+newstart0514@users.noreply.github.com>
2023-05-12 21:11:46 +0800

    Update main.js
    更新项目启动描述

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ const { APP_PORT } = require('./config/config.default')
 
 const app = require('./app')
 app.listen(APP_PORT, () => {
-    console.log(`服务已经正常启动在：http://localhost:${APP_PORT}`)
+    console.log('🚀 项目极速启动完毕！')
+    console.log('😎 我是幽离，一个热爱程序开发以及网络安全的小菜狗')
+    console.log('😍 如果您喜欢本项目，请为本项目点上个star🌟吧')
+    console.log(`🎉 服务已经正常启动在🌐http://localhost:${APP_PORT}`)
+    console.log('🥳 欢迎您使用本项目，项目正常运行ing')
+    console.log('🤖 正在尝试连接数据库...')
 })
